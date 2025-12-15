bfa0a804888b9896f377156c5f7b88d25c8830bd
mozrin <mozrin@gmail.com>
2025-12-15 09:17:52 +0700

    feat: Phase 004 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,41 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:grimreach_api/player.dart';
+import 'package:grimreach_api/protocol.dart';
+import 'package:grimreach_api/messages.dart';
+import 'package:grimreach_api/message_codec.dart';
 
-void main() {
-  final player = Player(id: "test", x: 0, y: 0);
-  print('Client: API linked. Player created: ${player.id}');
+void main() async {
   runApp(const Placeholder());
+
+  try {
+    final socket = await WebSocket.connect('ws://localhost:8080/ws');
+    final codec = MessageCodec();
+
+    print('Client: Connected to server');
+
+    socket.listen(
+      (data) {
+        if (data is String) {
+          final msg = codec.decode(data);
+          print('Client: Echo received: ${msg.type}');
+        }
+      },
+      onError: (e) {
+        print('Client: Error: $e');
+      },
+      onDone: () {
+        print('Client: Disconnected');
+      },
+    );
+
+    // Send handshake
+    final handshake = Message(
+      type: Protocol.handshake,
+      data: {'id': 'client_1'},
+    );
+    socket.add(codec.encode(handshake));
+    print('Client: Sent handshake');
+  } catch (e) {
+    print('Client: Connection failed: $e');
+  }
 }
