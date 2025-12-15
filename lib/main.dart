4fe85da08ee1a2108018a16e3814288bf7d03b7b
mozrin <mozrin@gmail.com>
2025-12-15 09:14:27 +0700

    feat: Phase 003 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:grimreach_api/player.dart';
 
+void main() {
+  final player = Player(id: "test", x: 0, y: 0);
+  print('Client: API linked. Player created: ${player.id}');
+  runApp(const Placeholder());
+}
